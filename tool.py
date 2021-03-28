f47376f140b84ce80f7ff257c2a255ccad7cb520
UnderAnder <andryushin.alexander@gmail.com>
2021-03-28 14:29:15 +0700

    menu refactoring

--- tool.py
+++ tool.py
@@ -4,91 +4,100 @@ from db import DBWorker
 class Memo:
     def __init__(self) -> None:
         self.run = True
-        self.flashcards = Flashcards(self)
-        self.menu = Menu(self)
-        self.state = self.menu.main_menu
 
     def start(self) -> None:
+        main_menu = self.init_menu()
+
         while self.run:
-            self.state()
+            main_menu()
+
+    def init_menu(self):
+        main_menu = Menu('main', back=True)
+        add_menu = Menu('add', back=True)
+        
+        main_add = Menu('Add flashcards', callback=add_menu)
+        main_practice = Menu('Practice flashcards', callback=Flashcards.practice)
+        main_exit = Menu('Exit', callback=self.exit_)
+        
+        add_menu_add = Menu('Add a new flashcard', master=add_menu, callback=Flashcards.add_new)
+        add_menu_exit = Menu('Exit', callback=main_menu)
+        
+        main_menu.add_options(main_add, main_practice, main_exit)
+        add_menu.add_options(add_menu_add, add_menu_exit)
+        return main_menu
 
     def exit_(self) -> None:
         print('Bye!')
         self.run = False
 
-    def change_state(self, func):
-        self.state = func
-
 
 class Menu:
-    def __init__(self, memo: Memo) -> None:
-        self.memo = memo
+    def __init__(self, name: str, options: list = None, master=None, callback=None, back: bool = False) -> None:
+        self.options = options if options is not None else []
+        self.name = name
+        self.master = master
+        self.callback = callback
+        self.back = back
 
-    def main_menu(self) -> None:
-        options = {'Add flashcards': self.add_menu,
-                   'Practice flashcards': self.memo.flashcards.practice,
-                   'Exit': self.memo.exit_}
-        self.print_options(options)
-        self.select_option(options)
+    def __call__(self) -> None:
+        if self.callback:
+            if isinstance(self.callback, (list, tuple)):
+                self.callback[0](*self.callback[1::])
+            elif callable(self.callback):
+                self.callback()
+            else:
+                raise ValueError("Callback is not a list, tuple or callable")
+        if self.options:
+            for i, option in enumerate(self.options, start=1):
+                print(f"{i}. {option.name}")
+            allowed = tuple(str(i) for i in range(1, len(self.options) + 1))
+            user_choice = check_input(proper_values=allowed, message='is not an option', back=self.back)
+            if not user_choice:
+                self()
+            self.options[int(user_choice) - 1]()
+        else:
+            if self.master:
+                self.master()
 
-    def add_menu(self) -> None:
-        options = {'Add a new flashcard': self.memo.flashcards.add_new,
-                   'Exit': self.main_menu}
-        self.print_options(options)
-        self.select_option(options)
-
-    @staticmethod
-    def print_options(options: dict) -> None:
-        print('\n'.join(f'{i}. {x}' for i, x in enumerate(options, start=1)))
-
-    def select_option(self, options: dict) -> None:
-        allowed = [str(i) for i in range(1, len(options) + 1)]
-        user_input = check_input(proper_values=allowed, message='is not an option', back=True)
-        if not user_input:
-            return
-        self.memo.change_state(options.get(list(options.keys())[int(user_input) - 1]))
+    def add_options(self, *options):
+        self.options += options
 
 
 class Flashcards:
-    def __init__(self, memo: Memo) -> None:
-        self.memo = memo
-
-    def add_new(self) -> None:
+    @classmethod
+    def add_new(cls) -> None:
         question, answer = None, None
         while not question:
             question = input('Question:\n')
         while not answer:
             answer = input('Answer:\n')
         DBWorker.add(question, answer)
-        self.memo.change_state(self.memo.menu.add_menu)
 
-    def practice(self) -> None:
+    @classmethod
+    def practice(cls) -> None:
         rows = DBWorker.get_all()
         if not rows:
             print('There is no flashcard to practice!')
-            self.memo.change_state(self.memo.menu.main_menu)
             return
 
         for row in rows:
             allowed = ('y', 'n', 'u')
             input_text = 'press "y" to see the answer:\npress "n" to skip:\npress "u" to update:\n'
             print('Question:', row.question)
-            user_input = check_input(input_text, proper_values=allowed, message='is not an option')
-            if user_input == 'y':
+            user_choice = check_input(input_text, proper_values=allowed, message='is not an option')
+            if user_choice == 'y':
                 print('Answer:', row.answer, '\n')
-            elif user_input == 'u':
-                self.change(row)
-
-        self.memo.change_state(self.memo.menu.main_menu)
+            elif user_choice == 'u':
+                cls.change(row)
 
     @staticmethod
     def change(row):
         allowed = ('d', 'e')
         input_text = 'press "d" to delete the flashcard:\npress "e" to edit the flashcard:\n'
-        user_input = check_input(input_text, proper_values=allowed, message='is not an option')
-        if user_input == 'e':
+        user_choice = check_input(input_text, proper_values=allowed, message='is not an option')
+        if user_choice == 'e':
             Flashcards.edit(row)
-        if user_input == 'd':
+        if user_choice == 'd':
             DBWorker.delete(row)
 
     @staticmethod
@@ -104,7 +113,7 @@ class Flashcards:
         DBWorker.edit(row, question, answer)
 
 
-def check_input(*args, proper_values=None, message=None, back=False, **kwargs):
+def check_input(*args, proper_values: tuple = None, message: str = None, back: bool = False, **kwargs):
     if proper_values is None:
         raise ValueError()
     while True:
